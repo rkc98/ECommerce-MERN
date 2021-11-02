213c1d0771fad02677703306bccebf56654fd4dc
Rahul <rkc.98@outlook.com>
2021-11-02 17:41:55 +0530

    product model added

---
+++ Backend/models/productModel.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please enter product Name"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: [true, "please enter description"],
+  },
+  price: {
+    type: Number,
+    required: [true, "please enter price"],
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, "please enter category"],
+  },
+  stock: {
+    type: Number,
+    required: [true, "please enter the stock"],
+    maxlength: [4, "stock cannot exceed 4 characters"],
+    default: 1,
+  },
+  numberOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  reviews: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Product", productSchema);
