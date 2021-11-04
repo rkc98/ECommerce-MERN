afaa216540877a3c6d2e147c767c13e1e46df937
Rahul <rkc.98@outlook.com>
2021-11-05 05:20:47 +0530

    added comments to routes

--- Backend/routes/productRoute.js
+++ Backend/routes/productRoute.js
@@ -13,26 +13,26 @@ const { authenticateUser, authorizeRoles } = require("../middlewares/auth");
 
 const router = express.Router();
 
-router.route("/products").get(getAllProducts);
+router.route("/products").get(getAllProducts); //get All products
 
 router
   .route("/products/new")
-  .post(authenticateUser, authorizeRoles("admin"), createProduct);
+  .post(authenticateUser, authorizeRoles("admin"), createProduct); // create product (Admin only)
 
 router
   .route("/product/:id")
-  .put(authenticateUser, authorizeRoles("admin"), updateProduct);
+  .put(authenticateUser, authorizeRoles("admin"), updateProduct); // update product (Admin only)
 
 router
   .route("/product/:id")
-  .delete(authenticateUser, authorizeRoles("admin"), deleteProduct);
+  .delete(authenticateUser, authorizeRoles("admin"), deleteProduct); // delete product (Admin only)
 
-router.route("/product/:id").get(getProductDetails);
+router.route("/product/:id").get(getProductDetails); // get a single products details
 
-router.route("/review").put(authenticateUser, createProductReview);
+router.route("/review").put(authenticateUser, createProductReview); // add product review
 
-router.route("/reviews").get(getAllReviews);
+router.route("/reviews").get(getAllReviews); //get all reviews for a product
 
-router.route("/reviews").delete(authenticateUser, deleteReview);
+router.route("/reviews").delete(authenticateUser, deleteReview); // delete a review
 
 module.exports = router;
--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -17,28 +17,28 @@ const { authenticateUser, authorizeRoles } = require("../middlewares/auth");
 
 const router = express.Router();
 
-router.route("/register").post(registerUser);
-router.route("/login").post(loginUser);
-router.route("/logout").get(logoutUser);
-router.route("/password/forgot").post(forgotPassword);
-router.route("/password/reset/:token").put(resetPassword);
-router.route("/me").get(authenticateUser, getUserDetails);
-router.route("/password/update").put(authenticateUser, updatePassword);
-router.route("/me/update").put(authenticateUser, updateUserDetails);
+router.route("/register").post(registerUser); // register user
+router.route("/login").post(loginUser); // login user
+router.route("/logout").get(logoutUser); // logout user
+router.route("/password/forgot").post(forgotPassword); // forgot password
+router.route("/password/reset/:token").put(resetPassword); // reset Password
+router.route("/me").get(authenticateUser, getUserDetails); // get logged in user details (Profile)
+router.route("/password/update").put(authenticateUser, updatePassword); // update your password
+router.route("/me/update").put(authenticateUser, updateUserDetails); // update user details
 router
   .route("/getAllUsers")
-  .get(authenticateUser, authorizeRoles("admin"), getAllUsers); // Admin only route
+  .get(authenticateUser, authorizeRoles("admin"), getAllUsers); //get all users (Admin only route)
 
 router
   .route("/getUser/:id")
-  .get(authenticateUser, authorizeRoles("admin"), getUser); // Admin only route
+  .get(authenticateUser, authorizeRoles("admin"), getUser); //get single user (Admin only route)
 
 router
   .route("/updateUser/:id")
-  .put(authenticateUser, authorizeRoles("admin"), updateUserRole); // Admin only route
+  .put(authenticateUser, authorizeRoles("admin"), updateUserRole); // update user roles (Admin only route)
 
 router
   .route("/deleteUser/:id")
-  .delete(authenticateUser, authorizeRoles("admin"), deleteUser); //Admin only route
+  .delete(authenticateUser, authorizeRoles("admin"), deleteUser); // delete user roles (Admin only route)
 
 module.exports = router;
