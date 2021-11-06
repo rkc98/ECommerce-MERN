9c3f27d112a84211b20a37be5032232ac8fdb2ec
Rahul <rkc.98@outlook.com>
2021-11-07 01:37:13 +0530

    added loading component

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Styles from './Loading.module.css'
+const Loading = () => {
+    return (
+        <div className={Styles.loading}>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Loading
